ad82fb18661320e641880cf97c8f645a98fdf97f
yuu655 <y09768591@gmail.com>
2026-02-13 13:33:12 +0900

    error wo naosita

--- src/app/articles/components/button.js
+++ src/app/articles/components/button.js
@@ -2,7 +2,7 @@
 
 export default function PreviewButton() {
   const endPreview = async () => {
-    await fetch('/api/exit-draft');
+    await fetch('/api/exit-draft?redirect=/articles/');
   }
   return (<button onClick={endPreview}>Previewを終了</button>);
 }
